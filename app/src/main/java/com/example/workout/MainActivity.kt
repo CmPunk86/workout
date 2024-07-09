f52a92064dd6102c06b389a8915a3c968ea4aff5
Petrucho1986 <vasukov1986@gmail.com>
2024-07-09 11:43:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/workout/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.workout
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.databinding.DataBindingUtil
+import com.example.workout.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.chip.setOnClickListener { changeText() }
+    }
+
+    private fun changeText() {
+        val myTextView : TextView = findViewById(R.id.textView)
+        myTextView.text = "Data Binding"
+
+    }
+
+}
